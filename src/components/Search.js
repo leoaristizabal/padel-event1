9e507960265b92754afcf00ecaa944bceaa0cdf1
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2024-01-26 01:48:03 -0400

    cambio de orden de los menus desplegables en el buscador

--- src/components/Search.js
+++ src/components/Search.js
@@ -18,9 +18,9 @@ const Search = () => {
   return (
     <div className="px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4 lg: shadow-1 bg-white lg:bg-transparent lg:backdrop-blur rounded-lg ">
       {/* Componentes de dropdown para ubicación, mes y categoría */}
-      <ClubDropdown />
-      <MesDropdown />
-      <CategoriaDropdown />
+      <ClubDropdown /> {/*location */}
+      <CategoriaDropdown /> {/*prop*/}
+      <MesDropdown />   {/*pric*/}
 
       {/* Botón de búsqueda con icono */}
       <button className="bg-green-700 hover:bg-green-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg">
