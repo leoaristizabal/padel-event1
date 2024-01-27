351e730ea4ecab12ad2cca255e57b05efdccbbda
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2024-01-27 03:30:27 -0400

    agregar useEffect con mapeo para encontrar las categorias y los meses de torneos y returnalos

--- src/components/TorneoContext.js
+++ src/components/TorneoContext.js
@@ -10,14 +10,15 @@ export const TorneoContext = createContext();
 const TorneoContextProvider = ({ children }) => {
   // Estados para almacenar datos importantes que necesito
   const [torneos, setTorneos] = useState(torneosData); // Datos de torneos
-  const [club, setClub] = useState("Club (Todos)"); // Nombre del club seleccionado
+  const [club, setClub] = useState("Clubes Disponibles"); // Nombre del club seleccionado
   const [clubes, setClubes] = useState([]); // Conjunto de opciones de clubes
-  const [categoria, setCategoria] = useState("Categoria (Todas)"); // Categoría seleccionada
+  const [categoria, setCategoria] = useState("Categorias Disponibles"); // Categoría seleccionada
   const [categorias, setCategorias] = useState([]); // Conjunto de opciones de categorías
-  const [mes, setMes] = useState("Meses (Todos)"); // Mes seleccionado
-  const [loading, setLoading] = useState(false); // Indicador de carga
+  const [mes, setMes] = useState("Mes"); // Mes seleccionado
+  const [meses, setMeses] = useState([]); //Conjunto de opcoines de meses de los torneos
+  const [loading, setLoading] = useState(false); // Indicador de carga para el menu desplegable
 
-  //return de todos los clubes en la data.
+  //return de todos los CLUBES en la data. (useEffecto porque solo renderiza esta parte)
   useEffect(() => {
     const allClubes = torneos.map((torneo) => { //mapeo para encontrar cada torneo por separado del array Torneos data llamandolo solo torneo
       return torneo.club; //despues devuelve el club de cada objeto por serado denominado torneo.
@@ -26,16 +27,45 @@ const TorneoContextProvider = ({ children }) => {
 
     //En caso de haber clubes duplicados, los borramos/removemos (por ahora no hay):
 
-    const clubesUnicos = ["Clubes (Todos)", ...new Set(allClubes)];
+    const clubesUnicos = ["Clubes Disponibles (Todos)", ...new Set(allClubes)];
 
     {
       /*console.log(clubesUnicos); //Prubea de que este funicionando*/
     }
 
-    setClubes(clubesUnicos);
+    setClubes(clubesUnicos); //actualizar estado de Clubes a ClubesUnicos
   }, []);
 
-  //COnfigurar estados 'states' de los clubes'
+  //Return de las CATEGORIAS en la data (analogamente como con los clubes arriba)
+  useEffect(() => {
+    const allCategorias = torneos.map((torneo)=> { //mapeo para encontrar cada torneo por separado en el array de torneos.
+      return torneo.categoria; //devuelvo la categoria de cada objeto por separado.
+    });
+
+    console.log(allCategorias); 
+
+    const categoriaUnicas = ["Categorias Disponibles (Todos)", ... new Set(allCategorias)];
+
+    setCategorias(categoriaUnicas);
+  }, []);
+
+
+  //Return de los MESES de torneos disponibles (igual que con los Clubes y Categorias)
+
+  useEffect (()=> {
+    const allMeses = torneos.map((torneo) => {
+      return torneo.mes; 
+    });
+
+    console.log(allMeses);
+
+    const mesesUnicos = ["Mes (Todos)", ... new Set(allMeses)];
+
+    setMeses(mesesUnicos);
+
+    console.log(mesesUnicos);
+
+  }, []);
 
   // Devuelve el componente TorneoContext.Provider con los valores que queremos compartir
   return (
@@ -50,6 +80,7 @@ const TorneoContextProvider = ({ children }) => {
         categorias, // Conjunto de opciones de categorías
         mes, // Valor actual del mes
         setMes, // Función para actualizar el mes
+        meses, //conjunto de opciones de meses pero sin orden
         torneos, // Datos de torneos
         loading, // Estado de carga
       }}
